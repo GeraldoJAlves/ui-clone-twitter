608a3bbb46fb7977f9f7cb9c33c61ce63d63afa5
Geraldo Alves <junior.alves.2007@gmail.com>
2020-08-22 18:51:49 -0300

    Profile data

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Banner, ProfileData, Avatar, LocationIcon, CakeIcon, FollowAge } from './styles';
 
 const ProfilePage: React.FC = () => {
-  return <div />;
+
+  return (
+  <Container>
+    <Banner>
+      <Avatar />
+    </Banner>
+    <ProfileData>
+      {/* <EditButton outilned>Editar Perfil</EditButton> */}
+      <h1>Geraldo Alves</h1>
+      <h1>@geraldoalves</h1>
+      <p>Developer at <a href='4yousee.com.br'>4yousee</a></p>
+      <ul>
+        <li>
+          <LocationIcon />
+          Belo Horizonte
+        </li>
+        <li>
+          <CakeIcon />
+          Nascido em 15 de fevereiro de 1994
+        </li>
+      </ul>
+      <FollowAge>
+        <span>
+          seguindo <strong>100</strong>
+        </span>
+        <span>
+          <strong>50</strong> seguidores
+        </span>
+      </FollowAge>
+    </ProfileData>
+  </Container>
+  );
 }
 
 export default ProfilePage;
--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -1,5 +1,48 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import {Cake, LocationOn} from '../../styles/Icons';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  max-height: 100%;
+  overflow-y: auto;
+  scrollbar-width: none;
+  ::webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const Banner = styled.div`
+  flex-shrink: 0;
+
+  width: 100%;
+  height: min(33vw, 199px);
+`;
+
+export const Avatar = styled.div`
   
+`;
+
+export const FollowAge = styled.div`
+  
+`;
+
+export const ProfileData = styled.div`
+  
+`;
+
+const cssIcon = css`
+  width: 20px;
+  height: 20px;
+  fill: var(--gray);
+`;
+
+export const CakeIcon = styled(Cake)`
+  ${cssIcon}
+`;
+
+export const LocationIcon = styled(LocationOn)`
+  ${cssIcon}
 `;
