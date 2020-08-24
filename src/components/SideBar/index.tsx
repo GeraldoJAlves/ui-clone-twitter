a2988b942b8d83922858461f901be6d27fe78230
Geraldo Alves <junior.alves.2007@gmail.com>
2020-08-24 02:00:32 -0300

    finish project

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import StickyBox from 'react-sticky-box';
+
 import {
   Container,
   SearchWrapper,
@@ -8,7 +10,9 @@ import {
   Body,
 } from './styles';
 
-import { List } from '../List';
+import List from '../List';
+import FollowSuggestion from '../FollowSuggestion';
+import News from '../News';
 
 const SideBar: React.FC = () => {
   return (
@@ -17,18 +21,81 @@ const SideBar: React.FC = () => {
         <SearchInput placeholder="Buscar no Twitter" />
         <SearchIcon />
       </SearchWrapper>
-      <Body>
-        <p>
-          Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros
-          commodo tempor.
-          Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Atirei o pau no gatis, per gatis num morreus. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Si num tem leite então bota uma pinga aí cumpadi!
-          Diuretics paradis num copo é motivis de denguis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Casamentiss faiz malandris se pirulitá.
-          A ordem dos tratores não altera o pão duris. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Per aumento de cachacis, eu reclamis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.
-          Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Atirei o pau no gatis, per gatis num morreus. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Si num tem leite então bota uma pinga aí cumpadi!
-          Diuretics paradis num copo é motivis de denguis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Casamentiss faiz malandris se pirulitá.
-          A ordem dos tratores não altera o pão duris. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Per aumento de cachacis, eu reclamis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.
-        </p>
-      </Body>
+      <StickyBox>
+        <Body>
+          <List
+            title="Talvez você curta"
+            elements={[
+              <FollowSuggestion name="Maria Alves" nickname="@maria" />,
+              <FollowSuggestion name="Jose Gomes" nickname="@jose" />,
+              <FollowSuggestion name="Joao Fino" nickname="@joao" />,
+            ]}
+          />
+          <List
+            title="Top Trends"
+            elements={[
+              <News
+                title="Assuntos do momento no mundo"
+                description="Covid-19"
+              />,
+              <News
+                title="Assuntos do momento no brasil"
+                description="ui clone twitter"
+              />,
+              <News
+                title="Assuntos do momento em minas"
+                description="pao de queijo mais barato"
+              />,
+              <News
+                title="Assuntos do momento em abaete"
+                description="rua cheia de buracos"
+              />,
+            ]}
+          />
+          <List
+            title="Top Trends"
+            elements={[
+              <News
+                title="Assuntos do momento no mundo"
+                description="Covid-19"
+              />,
+              <News
+                title="Assuntos do momento no brasil"
+                description="ui clone twitter"
+              />,
+              <News
+                title="Assuntos do momento em minas"
+                description="pao de queijo mais barato"
+              />,
+              <News
+                title="Assuntos do momento em abaete"
+                description="rua cheia de buracos"
+              />,
+            ]}
+          />
+          <List
+            title="Top Trends"
+            elements={[
+              <News
+                title="Assuntos do momento no mundo"
+                description="Covid-19"
+              />,
+              <News
+                title="Assuntos do momento no brasil"
+                description="ui clone twitter"
+              />,
+              <News
+                title="Assuntos do momento em minas"
+                description="pao de queijo mais barato"
+              />,
+              <News
+                title="Assuntos do momento em abaete"
+                description="rua cheia de buracos"
+              />,
+            ]}
+          />
+        </Body>
+      </StickyBox>
     </Container>
   );
 };
